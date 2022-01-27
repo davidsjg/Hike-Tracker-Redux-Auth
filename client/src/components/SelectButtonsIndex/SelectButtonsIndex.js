35eb2f7c5ad3253482058efaca57134ac129991e
Joey Davidson <j.davidson18@gmail.com>
2022-01-27 09:46:25 -0700

    home index image added

--- client/src/components/SelectButtonsIndex/SelectButtonsIndex.js
+++ client/src/components/SelectButtonsIndex/SelectButtonsIndex.js
@@ -1,8 +1,13 @@
 import React from "react";
+import MainContain from "../MainContain/MainContain";
 import styles from "./SelectButtonsIndex.module.css";
 
 function SelectButtonsIndex() {
-  return <div>sup yall</div>;
+  return (
+    <MainContain cName={styles["mainContain"]}>
+      <div className={styles["indexImage"]}></div>
+    </MainContain>
+  );
 }
 
 export default SelectButtonsIndex;
