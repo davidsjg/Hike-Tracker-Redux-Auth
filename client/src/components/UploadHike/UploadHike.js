a840adf284f56b73fc7ff927ad8be5886e141e4e
Joey Davidson <j.davidson18@gmail.com>
2022-01-28 10:04:52 -0700

    uploadHike page started, inputs and componet state done

--- client/src/components/UploadHike/UploadHike.js
+++ client/src/components/UploadHike/UploadHike.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./UploadHike.module.css";
+
+import MainContain from "../MainContain/MainContain";
 
 function UploadHike() {
-  return <div> Upload hike</div>;
+  const [formData, setFormData] = useState({
+    distance: 0,
+    time: 0,
+    rating: 0,
+  });
+
+  useEffect(() => {
+    setFormData({
+      distance: "",
+      time: "",
+      rating: "",
+    });
+  }, []);
+
+  const handleClick = () => {
+    console.log(formData);
+  };
+
+  return (
+    <MainContain cName={styles["mainContain"]}>
+      <div className={styles["formHolder"]}>
+        <form className={styles["inputForm"]}>
+          <div className={styles["inputParams"]}>
+            <p>Distance:</p>
+            <p>Time:</p>
+            <p>Rating:</p>
+          </div>
+          <div className={styles["inputData"]}>
+            <input
+              value={formData.distance}
+              onChange={(e) =>
+                setFormData({ ...formData, distance: e.target.value })
+              }
+            />
+            <input
+              value={formData.time}
+              onChange={(e) =>
+                setFormData({ ...formData, time: e.target.value })
+              }
+            />
+            <input
+              value={formData.rating}
+              onChange={(e) =>
+                setFormData({ ...formData, rating: e.target.value })
+              }
+            />
+          </div>
+        </form>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+    </MainContain>
+  );
 }
 
 export default UploadHike;
